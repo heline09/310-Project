889e07cb781f68efc804d1b65a482ee9d52333d6
unknown <luisashikuku@gmail.com>
2023-02-24 12:54:48 +0300

    third commit

---
+++ TempConvComp/TempConvComp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace TempConv
+{
+   
+     public class TempConvComp
+        {
+            public TempConvComp()
+            {
+
+            }
+            public double cToF(double c)
+            {
+                return (int)((c * 9 / 5.0 + 32) * 100) / 100.0;
+            }
+            public double fToC(double f)
+            {
+                return (int)((f - 32) * 5 / 9.0 * 100) / 100.0;
+            }
+        }
+    }
